254b717eab146b43022dfbd896fa45d62c0d328b
Ashwin19v <158269761+Ashwin19v@users.noreply.github.com>
2024-07-21 19:23:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
     rollupOptions: {
-      external: []
-    }
-  }
+      // Specifies that react-router-dom should not be bundled with the application
+      external: ["react-router-dom"],
+    },
+  },
+  plugins: [
+    // Adds React support to Vite
+    react(),
+  ],
 });

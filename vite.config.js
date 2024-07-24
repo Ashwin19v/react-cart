7fad536a0998eeb6d4e6778a5e1b9d7b5ade0258
Ashwin19v <158269761+Ashwin19v@users.noreply.github.com>
2024-07-24 19:36:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
-  plugins: [react()],
-  server: {
-    open: true,
-  },
+ 
+  plugins: [react()]
 });

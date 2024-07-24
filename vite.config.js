1467ac98863731a1b145fa43731f5dfec353717a
Ashwin19v <158269761+Ashwin19v@users.noreply.github.com>
2024-07-24 19:39:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
  
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  }
 });
